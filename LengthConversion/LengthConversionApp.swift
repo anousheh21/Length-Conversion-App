d30b496ba01ca07f67f28d45fa7a91b094b55e6b
anousheh21 <anousheh1@gmail.com>
2023-12-24 11:53:07 +0000

    first commit

---
+++ LengthConversion/LengthConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConversionApp.swift
+//  LengthConversion
+//
+//  Created by Anousheh Hasan on 24/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct LengthConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
